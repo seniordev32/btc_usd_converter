1006db383ae8eccc137c9e5619e8c27fa3ecf06a
seniordev32 <senior.dev32@gmail.com>
2021-01-26 23:47:48 +1000

    basic ui

---
+++ BTC_USD/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BTC_USD
+//
+//  Created by Moon on 1/26/21.
+//  Copyright © 2021 Eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfBtcAmount: UITextField!
+    @IBOutlet weak var tfUsdAmount: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
